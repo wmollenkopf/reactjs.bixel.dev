1362ea79ce27af1371c11e6e4930c38af84e5d9f
wmollenkopf <12564813+wmollenkopf@users.noreply.github.com>
2020-07-18 22:53:07 -0400

    Updated method to test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders initial home page title', () => {
+  const { getByText } = render(<App />);
+  const testElement = getByText(/William.Mollenkopf/i);
+  expect(testElement).toBeInTheDocument();
 });
